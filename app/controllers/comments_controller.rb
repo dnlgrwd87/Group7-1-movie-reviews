2ae558e56f962afd2359c6620852c86591e43f92
Zak Shaver <zshaver1@uncc.edu>
2018-11-13 13:18:04 -0500

    Add comment controller and comments on show route;

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
     def create
-        @review = Reviews.find(params[:review_id])
+        @review = Review.find(params[:review_id])
         @comment = @review.comments.create(comment_params)
         redirect_to review_path(@review)
+    end
 
     private 
-    def comment_params
-        params.require(:comment).permit(:commenter, :body)
-
-    end 
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end 
 end
