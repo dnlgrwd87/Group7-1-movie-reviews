aa4738418b42598ce76f54153e088f4127520699
Daniel <dgarwood@uncc.edu>
2018-11-15 09:03:38 -0500

    Add destroy functionalirty to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to review_path(@review)
   end
 
+  def destroy
+    @review = Review.find(params[:review_id])
+    @comment = @review.comments.find(params[:id])
+    @comment.destroy
+    redirect_to review_path(@review)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:author, :body)
